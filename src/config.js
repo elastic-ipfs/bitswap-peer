4acfb33271fa8cce364550e752ed1115aa541e45
Shogun <ShogunPanda@users.noreply.github.com>
2021-12-03 14:36:01 +0100

    feat: Download peerId at runtime. (#4)

--- src/config.js
+++ src/config.js
@@ -3,20 +3,20 @@
 require('dotenv').config()
 const PeerId = require('peer-id')
 const { readFile } = require('fs/promises')
-const { resolve } = require('path')
+const { join } = require('path')
 
 const {
   CACHE_BLOCKS_INFO: cacheBlocksInfo,
   CONCURRENCY: rawConcurrency,
   DYNAMO_BLOCKS_TABLE: blocksTable,
   DYNAMO_CARS_TABLE: carsTable,
-  PEER_ID_JSON: peerIdJsonPath,
+  PEER_ID_FILE: peerIdJsonPath,
   PORT: rawPort
 } = process.env
 
 async function getPeerId() {
   try {
-    const peerIdJson = JSON.parse(await readFile(resolve(process.cwd(), peerIdJsonPath), 'utf-8'))
+    const peerIdJson = JSON.parse(await readFile(join(__dirname, '..', peerIdJsonPath), 'utf-8'))
     return await PeerId.createFromJSON(peerIdJson)
   } catch (e) {
     return PeerId.create()
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@
 
 require('make-promises-safe')
 
+const { writeFile } = require('fs/promises')
+const { join } = require('path')
+
 const { logger } = require('./logging')
 const { startService } = require('./service')
+const { fetchS3Object } = require('./storage')
 
-startService().catch(logger.error.bind(logger))
+async function downloadPeerIdFile() {
+  const contents = await fetchS3Object(process.env.PEER_ID_S3_BUCKET, process.env.PEER_ID_FILE)
+  return writeFile(join(__dirname, '..', process.env.PEER_ID_FILE), contents)
+}
+
+const before = process.env.PEER_ID_S3_BUCKET ? downloadPeerIdFile() : Promise.resolve()
+before.then(startService).catch(logger.error.bind(logger))

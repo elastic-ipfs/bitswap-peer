db24382f64e2f43b713f85053890b6c7fce62ab4
Shogun <ShogunPanda@users.noreply.github.com>
2021-12-16 13:30:06 +0100

    fix: Added error handling.

---
+++ playgrounds/multihash-to-cid.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const { base58btc: base58 } = require('multiformats/bases/base58')
+const { CID } = require('multiformats/cid')
+const { decode: digestDecode } = require('multiformats/hashes/digest')
+
+const digest = digestDecode(base58.decode(process.argv[2]))
+const codec = parseInt(process.argv[3])
+console.log(`Multihash HEX digest: ${Buffer.from(digest.digest).toString('hex')}`)
+console.log(`CID (v1): ${CID.create(1, codec, digest).toString()}`)

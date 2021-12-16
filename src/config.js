f0c3adca0eaf8e0b2a464bc126635af14ba0de80
Shogun <ShogunPanda@users.noreply.github.com>
2021-12-16 12:06:13 +0100

    chore: Added debugging message.

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ const PeerId = require('peer-id')
 const { readFile, writeFile } = require('fs/promises')
 const { join } = require('path')
 
+const { logger } = require('./logging')
 const { fetchS3Object } = require('./storage')
 
 const {
@@ -17,6 +18,8 @@ const {
 } = process.env
 
 async function downloadPeerIdFile() {
+  logger.info(`Downloading PeerId from s3://${process.env.PEER_ID_S3_BUCKET}/${process.env.PEER_ID_FILE}`)
+
   const contents = await fetchS3Object(process.env.PEER_ID_S3_BUCKET, process.env.PEER_ID_FILE)
   return writeFile(join(__dirname, '..', process.env.PEER_ID_FILE), contents)
 }

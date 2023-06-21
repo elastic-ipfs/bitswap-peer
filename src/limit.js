b4ec8d75e1864c5524f96842c0d10ac09f8a5b09
Oli Evans <oli@protocol.ai>
2023-06-21 11:25:31 +0100

    chore: add test for truncateWantlist
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- src/limit.js
+++ src/limit.js
@@ -8,8 +8,9 @@ export function truncateWantlist (entries, max = 500) {
   // boxo aims to send less than 16KiB messages...
   if (entries.length <= max) { return entries }
 
-  // Prefer high priority and cancel messages.
-  return entries.sort(entryPrioritySort).slice(0, max)
+  // Prefer high priority and cancel messages. Copy before mutating.
+  const sorted = [...entries].sort(entryPrioritySort)
+  return sorted.slice(0, max)
 }
 
 /**
@@ -18,7 +19,7 @@ export function truncateWantlist (entries, max = 500) {
  * @param {Entry} b
  */
 export function entryPrioritySort (a, b) {
-  const score = priority(b) - priority(b)
+  const score = priority(b) - priority(a)
   if (score === 0) {
     if (a.cancel && !b.cancel) { return -1 }
     if (!a.cancel && b.cancel) { return 1 }
